113f3306b96fb7a8be5e07a61e520357a561614f
SiropTime <sirop4beats@gmail.com>
2023-05-26 01:35:17 +0300

    [~] ggetting event loop

--- app/__init__.py
+++ app/__init__.py
@@ -99,7 +99,7 @@ class App:
         self._constant_uc = ConstantUseCase(
             const_repo=self._constant_repo
         )
-        asyncio.new_event_loop().run_until_complete(self._constant_uc.load())
+        asyncio.get_event_loop().run_until_complete(self._constant_uc.load())
 
         self._constant_handler = ConstantHandler(const_uc=self._constant_uc)
 
