a5d544712d97a9e49568eadc0888cdeca61b759b
SiropTime <sirop4beats@gmail.com>
2023-05-25 22:51:25 +0300

    [~] async

--- app/__init__.py
+++ app/__init__.py
@@ -114,8 +114,6 @@ class App:
         self.__app.include_router(self._constant_handler.router)
         self.__app.include_router(self._calc_handler.router)
 
-        return await self.a_get_app()
-
     def get_app(self) -> fastapi.FastAPI:
         return self.__app
 
